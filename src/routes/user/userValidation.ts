17d2196249d2c2a461114ebcd4ace3947c355ddb
kielon <mosadowski23@gmail.com>
2022-09-04 11:55:32 +0200

    added password validation

--- src/routes/user/userValidation.ts
+++ src/routes/user/userValidation.ts
@@ -5,13 +5,29 @@ import { EmailExists } from "./userQueries";
 
 const emailValidation = async (email: string) => {
   const check = await EmailExists(email);
-  console.log(check);
   if (check !== null)
     throw createError(400, "user with this e-mail already exists");
   return true;
 };
 
+const passwordValidation = (password: string) => {
+  if (password.length < 3 || password.length > 12) {
+    throw createError(
+      400,
+      "password length must be between 3 and 12 characters"
+    );
+  }
+  if (password === password.toLowerCase()) {
+    throw createError(400, "password must contain at least 1 uppercase letter");
+  }
+  if (!/\d/.test(password)) {
+    throw createError(400, "password must contain at least 1 number");
+  }
+  return true;
+};
+
 export const userValidation = async (userInfo: Register) => {
   await emailValidation(userInfo.email);
+  passwordValidation(userInfo.password);
   return true;
 };
