8f2009f9ba47040989ccc32d20df666599e05bca
kielon <mosadowski23@gmail.com>
2022-09-21 20:28:43 +0200

    changed database structure

--- src/routes/category/category.ts
+++ src/routes/category/category.ts
@@ -4,6 +4,6 @@ import { getCategories } from "./categoryQueries";
 export const category = () => {
   fastify.get("/category", async (request, response) => {
     const result = await getCategories();
-    response.code(200).send({ data: result });
+    response.code(200).send({ result });
   });
 };
